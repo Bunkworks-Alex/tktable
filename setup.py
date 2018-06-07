a02d7e7bade0aa77d901d50fe853c459e43c4164
Alex Horridge <alexhorridge@esdm.co.uk>
2018-06-07 10:00:16 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(name='tktable', version='0.1', description='Excel style tables for tkinter', url='',
+      author='Alex Horridge', author_email='alex.horridge93@gmail.com', license='BSD-3-Clause', packages=['tktable'],
+      zip_safe=False)
